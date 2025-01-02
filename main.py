f3ac38559b04b40d1d63d3e1a14dc48e425cfc02
Taynara Araújo de Assis <112909056+tay-assis@users.noreply.github.com>
2025-01-01 21:20:54 -0300

    Set up the files main.py and cartesian_plane.py. In the file cartesian_plane.py, the visibility curve was desabled.

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+# -----------------------------------------------------------------------------
+# Project: Centroid Detection 2D
 # Author: Taynara Araújo
 # Date: 2024-12-29
 # Description: This is the main file of the project.
 # It is responsible for starting the application.
+# -----------------------------------------------------------------------------
 
 from src.ui.cartesian_plane import CartesianPlane
 
 if __name__ == "__main__":
-    
+    """
+    ---------------------------------------------------------------------------
+    Main function 
+    ---------------------------------------------------------------------------
+    """
     x_axis = [-5, -4 ,-3 , -2, -1, 1, 2, 3, 4, 5]
     y_axis = [-5, -4 ,-3 , -2, -1, 1, 2, 3, 4, 5]
     x_label = "X Axis"
@@ -24,5 +31,5 @@ if __name__ == "__main__":
         window_title = window_title, 
         figsize = figsize
     )
-    
+
     cartesian_plane.show()
--- src/ui/cartesian_plane.py
+++ src/ui/cartesian_plane.py
@@ -1,10 +1,19 @@
+# -----------------------------------------------------------------------------
 # This file is responsible for creating the main window
 # Cartesian plane and its configuration.
+# -----------------------------------------------------------------------------
 
 import matplotlib.pyplot as plt
+from matplotlib.patches import Circle, Rectangle, Polygon
+from matplotlib.widgets import Button
 
 class CartesianPlane:
     def __init__(self, x_axis, y_axis, x_label, y_label, title, window_title, figsize):
+        """
+        ---------------------------------------------------------------------
+        Initialize the Cartesian Plane with your parameters.
+        ---------------------------------------------------------------------
+        """
         self.x_axis = x_axis
         self.y_axis = y_axis
         self.x_label = x_label
@@ -15,13 +24,36 @@ class CartesianPlane:
 
         # Plotting the Cartesian Plane
         self.figure, self.ax = plt.subplots(figsize=self.figsize)
-        self.ax.plot(self.x_axis, self.y_axis)
+        self.ax.plot(self.x_axis, self.y_axis, color="black", visible=False)
         self.ax.set_xlabel(self.x_label)
         self.ax.set_ylabel(self.y_label)
         self.ax.set_title(self.title)
+        self.ax.grid(True)
 
         # Setting the window title
         self.figure.canvas.manager.set_window_title(self.window_title)
+
+    # def toggle_curve(self):
+    #     """
+    #     ---------------------------------------------------------------------
+    #     Set up the visibiliy of the curve plot.
+    #     The curve will be shown if it doesn't exist.
+    #     This method is to inicialite the plot without curve.
+    #     ---------------------------------------------------------------------
+    #     """
+    #     if self.plot_line is None:
+    #         # Create curve if it doesn't exist
+    #         self.plot_line, = self.ax.plot(self.x_axis, self.y_axis, visible=True)
+    #         self.ax.legend()
+    #     else:
+    #         # Remove curve if it exists
+    #         self.plot_line.remove()
+    #         self.plot_line = None
+    #         self.ax.legend().remove()  # Remove the legend
+
+    #     self.figure.canvas.draw_idle()  # Update the window
+
+        
 
     def show(self):
         plt.show()
